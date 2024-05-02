4b964eb0e9318843545a78ae29f9b830774dda2b
Jorge Alfonso Bernal Roa <jbernal@facturecolombia.com>
2024-05-02 17:53:38 -0500

    Si

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+
+ export default defineConfig({
+  use: {
+    // All requests we send go to this API endpoint.
+    baseURL: 'https://apiv2-test.coordinadora.com/recogidas/cm-solicitud-recogidas-ms/solicitud-recogida ',
+    // extraHTTPHeaders: {
+    //   // We set this header per GitHub guidelines.
+    //   'Accept': 'application/vnd.github.v3+json',
+    //   // Add authorization token to all requests.
+    //   // Assuming personal access token available in the environment.
+    //   'Authorization': `token ${process.env.API_TOKEN}`,
+    // },
+  }
+   
+});
